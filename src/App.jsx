1c8141ff267c8872e85cb4fa52dc4f0fdbce7f66
LvnReyS16 <saavedra.lvnry@gmail.com>
2025-01-23 14:38:26 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import "./App.css"
+import { useState } from "react"
+
 function App() {
+  const [count, setCount] = useState(0)
+  let name = 'John Doe'
+
+  function handleButton(type) {
+    setCount(type === 'increment' ? count + 1 : count - 1)
+  }
+  
 
   return (
     <>
-    <h1>hello</h1>
-    <div ></div>
+    <div>{name}</div>
+    <div>{count}</div>
+    <button onClick={function () {
+      handleButton("increment")
+    }}>
+      Increment
+    </button>
+    <button onClick={function (){
+      handleButton("decrement")
+    }}>
+      Decrement
+    </button>
     </>
   )
 }
