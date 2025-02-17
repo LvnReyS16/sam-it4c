093f6f145929e3753f98edec31b6b6b483b89c60
LvnReyS16 <saavedra.lvnry@gmail.com>
2025-02-17 17:00:41 +0800

    changes from another dev

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-      THIS IS HOME NAV PAGE
+      THIS IS HOME NAV PAGE 2
 
       <NavLink to="/"> BUTTON TO HOME PAGE </NavLink>
     </div>
