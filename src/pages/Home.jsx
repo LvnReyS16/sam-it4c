bede857f8eb83463bfcc2602fdd756b7ac6517b9
LvnReyS16 <saavedra.lvnry@gmail.com>
2025-02-17 16:52:54 +0800

    I change the tile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-      THIS IS HOME PAGE
+      THIS IS HOME NAV PAGE
 
       <NavLink to="/"> BUTTON TO HOME PAGE </NavLink>
     </div>
