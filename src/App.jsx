bbce80278807fe75efa2014dda2e0105f9f261b4
LvnReyS16 <saavedra.lvnry@gmail.com>
2025-02-20 14:42:40 +0800

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import "./App.css";
-import Hero from './components/Hero'
-import Home from "./pages/Home"
+import Hero from './components/Hero';
+import Home from './pages/Home'
 function App() {
 
   return (
     <div>
       <Routes>
         <Route path="/" element={<Hero/>}/>
-        <Route path ="/home" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
       </Routes>
       
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
 
-import {NavLink} from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-      THIS IS HOME NAV PAGE 2
-
-      <NavLink to="/"> BUTTON TO HOME PAGE </NavLink>
+      THIS IS HOME PAGE
     </div>
-    
   )
 }
 
